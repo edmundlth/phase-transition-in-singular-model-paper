747c60ae378aba15207fbd9781cbd6a29164378a
edmundlth <edmundlth95@gmail.com>
2023-03-28 15:24:47 +1100

    Fixed haiku MLP rlct estimation script

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
 OUTDIRNAME="./outputs/test_$(date +"%Y%m%d_%H%M")/"
 
-python haiku_mlp_rlct_estimate.py --num-itemps 6 --num-posterior-samples 2000  --thinning 4 --num-warmup 1000 --num-chains 1 --num-training-data 2023 --a0 0.5 --b0 0.5 --sigma-obs 0.1 --prior-std 1.0 --prior-mean 0.0 --output_dir $OUTDIRNAME --rng-seed 42
+
+python haiku_mlp_rlct_estimate.py --num-itemps 8 --num-posterior-samples 2000  --thinning 4 --num-warmup 1000 --num-chains 6 --num-training-data 2023 --a0 0.5 --b0 0.5 --sigma-obs 0.1 --prior-std 1.0 --prior-mean 0.0 --output_dir $OUTDIRNAME --rng-seed 0
