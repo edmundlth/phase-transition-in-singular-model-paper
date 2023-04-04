09093125535884a274a7f9711b1b5a44178d486d
edmundlth <edmundlth95@gmail.com>
2023-04-04 11:42:20 +1000

    Notebook for aggregating and plotting SLURM job array output. Add better sbatch submission script.

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -1,10 +1,14 @@
-export CONFIGDIR=curated_experiments/energy_entropy_trend_in_n/1htanh_h4_20230401/
+export CONFIGDIR=curated_experiments/energy_entropy_trend_in_n/1htanh_h4_20230401
 export CONFIGPATH=$CONFIGDIR/config_list.json
+export SLURMLOGDIR=$CONFIGDIR/slurm_logs
 export OUTPUTDIR=$CONFIGDIR
-export JOBNAME="1htanh_h4_narrowband"
+export JOBNAME="1htanh_h4_20230401"
 export CPUPERTASK=6
 
 NUMEXPT=$(cat $CONFIGPATH | jq 'length')
 ENDINDEX=$(($NUMEXPT - 1))
 echo "Job array last index: $ENDINDEX"
-sed "s/ENDINDEX/$ENDINDEX/g; s/CPUPERTASK/$CPUPERTASK/g" job_array.slurm | sbatch
+echo "Slurm logs will be stored at: $SLURMLOGDIR"
+mkdir -p $SLURMLOGDIR
+
+sed "s|ENDINDEX|$ENDINDEX|g; s|CPUPERTASK|$CPUPERTASK|g; s|SLURMLOGDIR|$SLURMLOGDIR|g" job_array.slurm
