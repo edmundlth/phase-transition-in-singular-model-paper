c58b73793fa1de5ce13b2f5ad58548c28b0e64dd
edmundlth <edmundlth95@gmail.com>
2023-04-14 02:24:35 +1000

    update job submission script

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -1,9 +1,11 @@
-export CONFIGDIR=curated_experiments/energy_entropy_trend_in_n/1htanh_h10_20230404
+NAME="1htanh_h8_unreal"
+export CONFIGDIR=curated_experiments/energy_entropy_trend_in_n/$NAME
 export CONFIGPATH=$CONFIGDIR/config_list.json
 export SLURMLOGDIR=$CONFIGDIR/slurm_logs
 export OUTPUTDIR=$CONFIGDIR
-export JOBNAME="1htanh_h4_20230401"
+export JOBNAME=$NAME
 export CPUPERTASK=8
+
 
 NUMEXPT=$(cat $CONFIGPATH | jq 'length')
 ENDINDEX=$(($NUMEXPT - 1))
@@ -11,4 +13,4 @@ echo "Job array last index: $ENDINDEX"
 echo "Slurm logs will be stored at: $SLURMLOGDIR"
 mkdir -p $SLURMLOGDIR
 
-sed "s|ENDINDEX|$ENDINDEX|g; s|CPUPERTASK|$CPUPERTASK|g; s|SLURMLOGDIR|$SLURMLOGDIR|g" job_array.slurm
+sed "s|ENDINDEX|$ENDINDEX|g; s|CPUPERTASK|$CPUPERTASK|g; s|SLURMLOGDIR|$SLURMLOGDIR|g" job_array.slurm | sbatch
